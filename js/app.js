1f390f6b1861897b6ec8b2ba70f8a20c0a98c0cc
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-14 23:37:16 +0100

    create div and create date and setInterval

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+let date = new Date();
+setInterval(() => {
+
+},1000);
