7f682c5fa152d5744b381bc5a4fefc465bdf526e
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-16 14:09:04 +0100

    create objet litteral

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
-let date = new Date();
-setInterval(() => {
 
-},1000);
+let Time = {
+
+    test(){
+        setInterval(() => {
+            let date = new Date();
+            hours.style.transform = "translateY(-6vh) rotate(" + 30 * date.getHours() + "deg)";
+            minutes.style.transform ="translateY(-10vh) rotate(" + 6 * date.getMinutes() + "deg)";
+            seconds.style.transform ="translateY(-12vh) rotate(" + 6 * date.getSeconds() + "deg)";
+        },1000);
+    }
+};
+Time.test();
