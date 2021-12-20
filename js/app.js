815fcd5b891c4f213668d8021a1a8480210153b6
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-20 13:19:04 +0100

    redimension

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let Time = {
             let date = new Date();
             hours.style.transform = "translateY(-6vh) rotate(" + 30 * date.getHours() + "deg)";
             minutes.style.transform ="translateY(-10vh) rotate(" + 6 * date.getMinutes() + "deg)";
-            seconds.style.transform ="translateY(-12vh) rotate(" + 6 * date.getSeconds() + "deg)";
+            seconds.style.transform ="translateY(-10.5vh) rotate(" + 6 * date.getSeconds() + "deg)";
         },1000);
     }
 };
